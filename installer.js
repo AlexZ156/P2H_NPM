eff4fd25d958f4e5087fcd2070ced1e743466c95
Alex Z <alex.zolotarev156@gmail.com>
2016-10-24 22:13:52 +0300

    some test

--- installer.js
+++ installer.js
@@ -1,6 +1,7 @@
 'use strict';
 const inquirer = require('inquirer');
 const exec = require('child_process').exec;
+const del = require('del');
 const fs = require('fs');
 const path = require('path');
 let buildOptions = {};
@@ -29,10 +30,6 @@ const questions = {
 	}
 };
 
-const projectTypeQuestions = [
-	
-];
-
 function checkQuestion(answers) {
 	const newQuestion = questions[answers[Object.keys(answers)[0]]];
 	buildOptions = Object.assign(buildOptions, answers);
@@ -49,14 +46,46 @@ function downloadBuild() {
 	// console.log(buildOptions)
 }
 
-var qqqq = path.resolve(__dirname, 'markup')
-console.log('!!!!', qqqq)
-fs.stat(path.resolve(__dirname, 'qqq'), function(err, result) {
-	console.log(result)
-})
 fs.stat(path.resolve(__dirname, 'markup'), function(err, result) {
-	console.log(result)
-})
+	var child = exec('npm install');
+	child.stdout.pipe(process.stdout);
+	child.stderr.pipe(process.stderr);
+	child.on('exit', function() {
+		console.log(654654654)
+		// activeProcessIndex++;
+		// startProcess();
+	});
+
+
+	return
+	if (!result) {
+		let tempFolder = 'markup';
+		let child = exec('git clone https://github.com/AlexZ156/markup.git ' + '"' + tempFolder + '"');
+		child.stdout.pipe(process.stdout);
+		child.stderr.pipe(process.stderr);
+		child.on('exit', function() {
+			del.sync(tempFolder + '/.git');
+			let child = exec('npm i');
+			child.stdout.pipe(process.stdout);
+			child.stderr.pipe(process.stderr);
+			child.on('exit', function() {
+
+				console.log(1111111111)
+			});
+			console.log(222222222)
+		});
+	} else {
+		let child = exec(path.resolve(__dirname, 'markup') + ' npm i');
+		child.stdout.pipe(process.stdout);
+		child.stderr.pipe(process.stderr);
+		child.on('exit', function() {
+
+			console.log(1111111111)
+		});
+
+		console.log(333333, path.resolve(__dirname, 'markup') + ' npm i')
+	}
+});
 
 // inquirer.prompt(questions.start).then(checkQuestion);
 
